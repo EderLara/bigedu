62ead39fd09ac43afe4565b872137449acf49d74
aitageo <aitageo1980@hotmail.com>
2022-09-27 22:20:55 -0500

    rutas probadas

--- src/controllers/dimensiones.control.js
+++ src/controllers/dimensiones.control.js
@@ -18,32 +18,6 @@ function saveTipoUser(req, res){
     let params = req.body;
     let tipouser = new TipoUser();
 
-<<<<<<< HEAD
-    TipoUser.findOne({ nombre_tipoUsuario: params.nombre_tipouser }).exec(
-      (err, data) => {
-        console.log(data);
-        if (err) return res.status(500).send({ mensaje: mensajes.m500 });
-        if (!data || data.nombre_tipoUsuario != params.nombre_tipouser) {
-          // campos obligatorios:
-          if (params.nombre_tipouser) {
-            tipouser.nombre_tipoUsuario = params.nombre_tipouser;
-
-            tipouser.save((err, tipoStored) => {
-              if (err) throw err;
-              if (tipoStored) {
-                return res.status(200).send({ tipo_user: tipoStored });
-              }
-            });
-          } else {
-            return res.status(404).send({ mensaje: mensajes.m000 });
-          }
-        } else {
-          let msj ="El tipo de usuario " + params.nombre_tipouser + " ya existe";
-          return res.status(200).send({ mensaje: msj });
-        }
-      }
-    );
-=======
     TipoUser.findOne({nombre_tipoUsuario: params.nombre_tipouser}
                     ).exec((err, data)=>{
                         if (err) return res.status(500).send({ mensaje:mensajes.m500 });
@@ -79,7 +53,6 @@ function saveInstitucion(req, res) {
 
 function savePeriodo(req, res) {
     
->>>>>>> f0d0a5a03c8511d36f54cfdd93ee9f5b771ce90a
 }
 
 module.exports = {
